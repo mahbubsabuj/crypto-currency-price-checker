2cff16656719c42829f0af00a3dbbf75e11f89ff
mahbubsabuj <mahbubsabuj3@gmail.com>
2022-06-02 02:15:53 +0600

    docs: :memo: Edit README.md

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CryptoPriceChecker
 
 A Crypto price checker with custom pagination + filtering method.
+
+Live : <a href="https://angular-crypto-price-checker.netlify.app/"> </a>
--- src/app/services/crypto.service.ts
+++ src/app/services/crypto.service.ts
@@ -2,15 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ICryptoData } from '../models/crypto.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CryptoService {
   constructor(private httpClient: HttpClient) {}
-  baseURL: string =
-    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd';
   getCryptoData(): Observable<ICryptoData[]> {
-    return this.httpClient.get<ICryptoData[]>(this.baseURL, {});
+    return this.httpClient.get<ICryptoData[]>(environment.baseURL, {});
   }
 }
